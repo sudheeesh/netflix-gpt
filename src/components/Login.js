94ca0942b3c2973645561c6f0aa35e74e53257a3
sudheeesh <sudheeshvilla5@gmail.com>
2024-10-25 13:27:03 +0530

    everything

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+
+const Login = () => {
+  return (
+    <div>
+     <Header/>
+     <div>
+        
+     </div>
+    </div>
+  )
+}
+
+export default Login
